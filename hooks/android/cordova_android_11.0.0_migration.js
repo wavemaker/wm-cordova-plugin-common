7ba4004ff7f825f4ab9b42e452a4736f91202239
Srinivasa Rao Boyina <2254369+sboyina@users.noreply.github.com>
2022-10-18 11:56:49 +0530

    android builds are failing due to use of  compile instead of implementation in barcode scanner gradle.

--- hooks/android/cordova_android_11.0.0_migration.js
+++ hooks/android/cordova_android_11.0.0_migration.js
@@ -1,16 +1,21 @@
 var fs = require('fs');
 
 var projectDir = __dirname + '/../../../../';
-const barcodescannerGradlePath = `${projectDir}/platforms/android/phonegap-plugin-barcodescanner/pushnotifications-barcodescanner.gradle`
+const barcodescannerGradlePath = `${projectDir}/platforms/android/phonegap-plugin-barcodescanner`;
 
 const migration = () => {
     if (fs.existsSync(barcodescannerGradlePath)) {
-        let text = fs.readFileSync(barcodescannerGradlePath, "utf-8");
-        if (text) {
-            text = text.replace(/compile\(/g, 'implementation(');
-            fs.writeFileSync(barcodescannerGradlePath, text);
-            console.log('migrated barcode scanner path');
-        }
+        fs.readdirSync(barcodescannerGradlePath).map(file => {
+            if (file.endsWith('.gradle')) {
+                const filePath = barcodescannerGradlePath + '/' + file;
+                let text = fs.readFileSync(filePath, "utf-8");
+                if (text) {
+                    text = text.replace(/compile\(/g, 'implementation(');
+                    fs.writeFileSync(filePath, text);
+                    console.log('migrated barcode scanner gradle : ' + filePath);
+                }
+            }
+        });
     }
     console.log('migrated for cordova-android 11.0.0.');
 };
